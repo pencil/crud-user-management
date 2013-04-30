33c17dc5936fd2ca2e9d5e87a5520eedf2ceec32
Nils Caspar <ncaspar@me.com>
2013-04-30 20:08:34 +0200

    Inital version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrudUserManagement::Application.config.secret_token = '961d8362728952ee943c938da68194d43bb1dd5cdf75dfcf42c2abb5b248dbf4d98da5a1f110297bc252615c915a24a0a37ba87229d324761f05d9d695c421bd'
